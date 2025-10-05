f49997ece7eec5147719a7a854849f0beacc8cf2
Jaisankar Selvam <89118783+jaisankar07@users.noreply.github.com>
2025-10-05 22:47:20 +0530

    Updated-workflow

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,9 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
